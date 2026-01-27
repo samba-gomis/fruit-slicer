d59eba6b7a42a9d45b70ff0af556b265c7141218
mahira-manico <mahira.manico@laplateforme.io>
2026-01-27 16:05:25 +0100

    full class done

--- bomb.py
+++ bomb.py
@@ -1,12 +1,31 @@
 import pygame
+import random
 
 class Bomb:
-    def __init__(self, vitesse, gravite, x, y):
-        self.x=y
-        self.y=y
-        self.vitesse=vitesse
-        self.gravite=gravite
-        self.image=pygame.image.load("image.png").convert_alpha()
-        self.rect=self.image.get_rect()
-        self.rect.x=x
-        self.rect.y=y
+    def __init__(self, x):
+        self.x=x
+        self.y=0
+        self.letter="b"
+        self.radius=30
+        self.color=(0,0,0)
+        self.speed=random.randint(2,7)
+
+    def update(self):
+        self.y+=self.speed  
+        if self.y>600:
+            return True
+        return False
+    
+
+    def draw(self,screen):
+
+        pygame.draw.circle(screen,self.color,(self.x,self.y),self.radius)
+        font=pygame.font.Font(None,40)
+        letter=font.render(self.letter, True,(255,0,0))
+        letter_rect=letter.get_rect(center=(self.x,self.y))
+        screen.blit(letter,letter_rect)
+
+    def is_hit(self,key):
+        if key==self.letter:
+            return True
+        return False
